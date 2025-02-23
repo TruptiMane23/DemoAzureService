fd7c22fbfd6145a9db7b30ad25aac0371d7eb171
Trupti Mane <trupti@intern.boll-und-partner.com>
2025-02-23 17:55:10 +0100

    Update API added

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 DB_CONNECTION = os.getenv("AZURE_SQL_CONNECTION")
 # "DRIVER={ODBC Driver 17 for SQL Server};SERVER=tcp:taskdb-server.database.windows.net,1433;DATABASE=TaskDB;UID=TruptiMane23;PWD=Frankfurt@2020"
 
+
 # Home route
 @app.route("/")
 def home():
@@ -17,14 +18,14 @@ def home():
 def get_db_connection():
     try:
         conn = pyodbc.connect(DB_CONNECTION)
+        print("Database connection:", conn)
         return conn
     except Exception as e:
         print(f"Database connection error: {str(e)}")
         return None
 
 
-
-# Get all tasks
+# GET Request
 @app.route("/tasks", methods=["GET"])
 def get_tasks():
     conn = get_db_connection()
@@ -35,11 +36,12 @@ def get_tasks():
     return jsonify(tasks)
 
 
-# Add a new task
+# POST request
 @app.route("/tasks", methods=["POST"])
 def add_task():
     data = request.json
     task_name = data.get("task_name")
+    
 
     if not task_name:
         return jsonify({"error": "Task name is required"}), 400
@@ -51,6 +53,9 @@ def add_task():
     conn.close()
 
     return jsonify({"message": "Task added successfully"}), 201
+
+
+# Update request
 
 
 if __name__ == "__main__":
